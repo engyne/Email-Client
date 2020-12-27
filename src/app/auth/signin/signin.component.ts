c29baf6ac240e04cd7eb8457b863ed11d522dc7c
Ramiz Raja <codingcoin@gmail.com>
2020-12-27 01:22:43 +0100

    signin form added

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { UniqueUsername } from '../validators/unique-username';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  authForm = new FormGroup({
+    username: new FormControl('',
+    [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^[a-z0-9]+$/)
+    ]),
+    password: new FormControl('', 
+    [ Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),])
+  })
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.authForm.valid) {
+        this.authService.signin(this.authForm.value).subscribe({
+          next: () => {},
+          error: ({ error }) => {
+            if (error.username || error.password) {
+              this.authForm.setErrors({ credentials: true });
+            }
+          }
+        });
+     }
   }
 
 }
